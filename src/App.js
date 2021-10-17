edcd646db9d19cbb48cdf2a273ddefd2665de3d8
ItzOso <ortizoscjr@gmail.com>
2021-10-16 17:27:03 -0700

    changed api directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_BASE = "http://localhost:3001";
+const API_BASE = "https://mern-todolist-1.herokuapp.com";
 
 function App() {
     const [todos, setTodos] = useState([]);
